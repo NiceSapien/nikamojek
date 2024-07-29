e6ceb9bdacf96238d8985881e5f57a72bafcc9d8
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-07-29 19:34:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nika Mojek",
-  description: "The nika mojek bot - worse discord bot ever created.",
+  description: "The nika mojek bot - worst discord bot ever created.",
 };
 
 export default function RootLayout({
