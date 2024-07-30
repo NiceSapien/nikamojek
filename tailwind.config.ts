2688459ef34a92eabecf2b66f2ba93047a0d3a9c
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-07-30 08:47:31 +0000

    new domain, same name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config = {
     },
     extend: {
       backgroundImage: {
-        'meme': "url('/nikamojek/meme.gif')",
+        'meme': "url('/meme.gif')",
       },
       keyframes: {
         "accordion-down": {
