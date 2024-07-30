b2027d0ce468fbae8b5a84a2483afb581f5ecb7b
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2024-07-30 09:34:22 +0000

    i just did smth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Image from "next/image";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
@@ -17,8 +16,11 @@ export default function Home() {
 <div className="text-white bg-[#212121] h-[600vh]">
   <p>What you here for, dawg? Scroll up.</p>
 </div>
-<div className="text-white bg-[#212121]">
+<div className="text-white bg-[#212121] h-[150vh]">
   <p>Sussy baka UwU</p>
+  </div>
+  <div className="text-white bg-[#212121] justify-center align-center text-center h-[10vh] text-2xl">
+  <p>I am NOT weeb. I repeat, I am NOT.</p>
   </div>
 </main>
   );
